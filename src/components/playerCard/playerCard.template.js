b977442f0ec0a1b7bae84f87eba456b98afc7a6a
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2026-05-22 16:28:07 -0300

    ...

--- src/components/playerCard/playerCard.template.js
+++
@@ -1,6 +0,0 @@
-export function createPowerDots(count, max = 5) {
-    return Array.from({ length: max }, (_, i) => {
-        const filled = i < count ? "filled" : "empty";
-        return `<span class="dot ${filled}"></span>`;
-    }).join("");
-}
