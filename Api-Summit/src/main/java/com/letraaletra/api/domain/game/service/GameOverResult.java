eb090d1c3938fb71144962f2de574b62316c1627
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2026-06-16 15:08:22 -0300

    removed

--- Api-Summit/src/main/java/com/letraaletra/api/domain/game/service/GameOverResult.java
+++
@@ -1,9 +0,0 @@
-package com.letraaletra.api.domain.game.service;
-
-import com.letraaletra.api.domain.game.player.Player;
-
-public record GameOverResult(
-        boolean finished,
-        Player winner,
-        Player loser
-) {}
--- Api-Summit/src/main/java/com/letraaletra/api/presentation/dto/response/game/GameOverDTO.java
+++
@@ -1,9 +0,0 @@
-package com.letraaletra.api.presentation.dto.response.game;
-
-import com.letraaletra.api.presentation.dto.response.player.PlayerDTO;
-
-public record GameOverDTO(
-        PlayerDTO winner,
-        PlayerDTO loser
-) {
-}
